a5feadbca9c747e3373d03f019593f2751564034
Febrian Rendak <febriananugrah92@gmail.com>
2023-08-23 15:01:12 +0700

    refactor

---
+++ client/auth.go
@@ -0,0 +1,27 @@
+package client
+
+import "guacamole/vars"
+
+type Auth struct {
+	*Client
+}
+
+func (client *Client) Auth() *Auth {
+	return &Auth{
+		client,
+	}
+}
+
+func (auth *Auth) Token() (vars.TokenResp, error) {
+	respJson := vars.TokenResp{}
+	_, err := auth.c.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetFormData(map[string]string{
+			"username": auth.username,
+			"password": auth.password,
+		}).
+		SetSuccessResult(&respJson).
+		Post("/tokens")
+
+	return respJson, err
+}
