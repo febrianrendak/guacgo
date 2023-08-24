d05c6a89d8728010b4cd1a90f66f3ed3c3bcda67
Febrian Rendak <febriananugrah92@gmail.com>
2023-08-24 14:13:24 +0700

    add connectivity to user api

---
+++ client/user.go
@@ -0,0 +1,75 @@
+package client
+
+import "guacamole/vars"
+
+type User struct {
+	*Client
+}
+
+func (client *Client) User() *User {
+	return &User{
+		client,
+	}
+}
+
+func (u *User) List() (userList []vars.User, mapOfUsers map[string]vars.User, err error) {
+	userList = make([]vars.User, 0)
+	mapOfUsers = make(map[string]vars.User)
+
+	_, err = u.NewRequest().
+		SetSuccessResult(&mapOfUsers).
+		Get("/session/data/{data-source}/users")
+
+	for _, user := range mapOfUsers {
+		userList = append(userList, user)
+	}
+
+	return
+}
+
+func (u *User) Details(username string) (user vars.User, err error) {
+	_, err = u.NewRequest().
+		SetSuccessResult(&user).
+		SetPathParam("username", username).
+		Get("/session/data/{data-source}/users/{username}")
+
+	return
+}
+
+func (u *User) Create(username, guacEmailAddress, guacFullName, disabled, password string) (user vars.User, err error) {
+	_, err = u.NewRequest().
+		SetSuccessResult(&user).
+		SetBody(&vars.UserCreate{
+			UserBasic: vars.UserBasic{
+				Username: username,
+			},
+			Password: password,
+			Attributes: vars.UserAttributes{
+				GuacEmailAddress: guacEmailAddress,
+				GuacFullName:     guacFullName,
+				Disabled:         disabled,
+			},
+		}).
+		Post("/session/data/{data-source}/users")
+
+	return
+}
+
+func (u *User) Update(username, guacEmailAddress, guacFullName, disabled, password string) (err error) {
+	_, err = u.NewRequest().
+		SetPathParam("username", username).
+		SetBody(&vars.UserCreate{
+			UserBasic: vars.UserBasic{
+				Username: username,
+			},
+			Password: password,
+			Attributes: vars.UserAttributes{
+				GuacEmailAddress: guacEmailAddress,
+				GuacFullName:     guacFullName,
+				Disabled:         disabled,
+			},
+		}).
+		Put("/session/data/{data-source}/users/{username}")
+
+	return
+}
